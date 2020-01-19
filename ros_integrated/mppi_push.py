ca60b806072f20744f6a41eb4d50982d6367d70f
TAMS Lab <tams-lab@informatik.uni-hamburg.de>
2020-01-19 15:22:39 +0100

    mppi push priliminary

--- ros_integrated/mppi_push.py
+++ ros_integrated/mppi_push.py
@@ -16,7 +16,6 @@ from trajectory import Trajectory
 from mppi import MPPI
 
 STEP_LIMIT = 100
-<<<<<<< HEAD
 TIME_DURATION = 0.15
 VELOCITY_SCALE = 0.15
 STEP_ACTION = 0.025
@@ -33,40 +32,17 @@ def get_object_tool_pose():
 
         pose_object = np.concatenate([np.asarray(trans_object), np.asarray(rot_object)])
         pose_tool = np.asarray(trans_tool)[:2]
-=======
-TIME_DURATION = 0.5
-
-def get_object_tool_pose():
-    listener = tf.TransformListener()
-    try:
-        (trans_object,rot_object) = listener.lookupTransform('pushable_object_0', 'table_gym', rospy.Time(0))
-        (trans_tool,rot_tool) = listener.lookupTransform('tool', 'table_gym', rospy.Time(0))
-    except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
-        continue
->>>>>>> 42978e8b76f0396a314e22abf765ae1c55f26b31
 
     # print(pose_tool)
 
     return pose_object, pose_tool
 
-# def push_distance(pub, vel_msg):
-#     now = rospy.Time.now()
-#     target_time = now + rospy.Duration(TIME_DURATION)
-#     while(rospy.Time.now() < target_time):
-#         pub.publish(vel_msg)
-
-def push_distance(pub, vel_msg, pose_tool_, action):
-    action_distance = np.sqrt(np.square(action[0]) + np.square(action[1]))
-    pushing_distance = 0.0
-    rate = rospy.rate(10)
-    while(pushing_distance < action_distance):
+def push_distance(pub, vel_msg):
+    now = rospy.Time.now()
+    target_time = now + rospy.Duration(TIME_DURATION)
+    while(rospy.Time.now() < target_time):
         pub.publish(vel_msg)
-        _, pose_tool = get_object_tool_pose()
-        pushing_distance = np.sqrt(np.square(pose_tool[0] - pose_tool_[0]) + np.square(pose_tool[1] - pose_tool_[1]))
-        rate.sleep()
 
-
-<<<<<<< HEAD
     vel_msg_0 = geometry_msgs.msg.Vector3()
     vel_msg_0.x = 0.0
     vel_msg_0.y = 0.0
@@ -121,26 +97,13 @@ def mppi_push(pos_x = 0.3, pos_y = 0.7, theta = 30):
     goal_marker = set_goal_marker(x, y, theta)
     goal_marker_pub.publish(goal_marker)
 
-=======
-
-def mppi_push(pos_x = 0.2, pos_y = 0.2, theta = 1):
-    # init
-    rospy.init_node('mppi_push')
-    # tf listener
-    # listener = tf.TransformListener()
->>>>>>> 42978e8b76f0396a314e22abf765ae1c55f26b31
     # get pos information from environment
     pose_object, pose_tool = get_object_tool_pose() #np.array(7), np.array(2)
     print('get pose')
 
     mppi = MPPI(80, 3, STEP_ACTION)
 
-<<<<<<< HEAD
     mppi.trajectory_set_goal(pos_x, pos_y, theta)
-=======
-    mppi = MPPI(20, 3)
-    mppi.trajectory_clear()
->>>>>>> 42978e8b76f0396a314e22abf765ae1c55f26b31
     mppi.U_reset()
     mppi.trajectory_update_state(pose_object, pose_tool)
     K = mppi.get_K() # get sample nums
@@ -156,7 +119,6 @@ def mppi_push(pos_x = 0.2, pos_y = 0.2, theta = 1):
         # pose_object, pose_tool = get_object_tool_pose()
         mppi.compute_cost(step)
         target_action = mppi.compute_noise_action() # 5 lines
-<<<<<<< HEAD
         print('target action computed: ', target_action)
         # target_action = 0.03*np.clip(target_action, -1.0, 1.0)
         # print('target action clipped: ', target_action)
@@ -166,13 +128,9 @@ def mppi_push(pos_x = 0.2, pos_y = 0.2, theta = 1):
         vel_msg.x = VELOCITY_SCALE*costheta
         vel_msg.y = -VELOCITY_SCALE*sintheta
         # vel_msg = target_action/TIME_DURATION # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! need to be complete!!!!!!with clip
-=======
-        target_action = 0.03*np.clip(target_action, -1, 1)
-        vel_msg = target_action/TIME_DURATION # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! need to be complete!!!!!!with clip
->>>>>>> 42978e8b76f0396a314e22abf765ae1c55f26b31
         # publish action through tool_velocity_control topic
         pose_tool_ = copy.copy(pose_tool)
-        push_distance(vel_pub, vel_msg, pose_tool_, target_action)
+        push_distance(vel_pub, vel_msg)
 
         pose_object, pose_tool = get_object_tool_pose()
         real_action = pose_tool - pose_tool_
